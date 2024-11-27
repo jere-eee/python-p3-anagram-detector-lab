82aacae87d79b07c086364b1852b32944de5f700
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-27 10:54:52 +0300

    anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self.word = word
+
+    def match(self, list):
+        anagrams = []
+        for w in list:
+            if (sorted(w) == sorted(self.word)):
+                anagrams.append(w)
+        return anagrams
